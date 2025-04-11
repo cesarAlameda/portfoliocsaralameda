6a1b7f25f375b3604acd6311abdb10980ce06702
cesarAlameda <147344298+cesarAlameda@users.noreply.github.com>
2025-04-11 21:53:28 +0200

    Upgrade en componente projects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-    production: true
+    production: true,
+    emailjs: {
+      publicKey: 'RK0sh4m79M-ppCuJf',
+      serviceId: 'service_v6ea218',
+      templateId: 'template_nqx9obg'
+    }
   };
