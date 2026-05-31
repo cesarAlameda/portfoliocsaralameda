5363264948df2296840d00a00e2a4864bb3d9ad5
cesarAlameda <147344298+cesarAlameda@users.noreply.github.com>
2026-05-31 21:16:19 +0200

    fix: carousel translateX in pixels for correct mobile sliding

---
+++ _next/static/chunks/app/[locale]/not-found-4e476649287fe6b4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[224],{3624:(e,s,n)=>{Promise.resolve().then(n.bind(n,3691)),Promise.resolve().then(n.bind(n,8558)),Promise.resolve().then(n.bind(n,8713))}},e=>{e.O(0,[62,441,255,358],()=>e(e.s=3624)),_N_E=e.O()}]);
